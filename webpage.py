ef1d43d2c8eabc547e3d0a0db42a996cb78caeab
atlashale <connor.a.h.h@gmail.com>
2019-05-06 15:44:18 -0700

    fixed some styling caused by not pulling current code

--- webpage.py
+++ webpage.py
@@ -13,17 +13,20 @@ def hello():
 
 @app.route("/results", methods=['POST', 'GET'])
 def result():
-	if request.method == 'POST':
-		text = request.form['lang']
-		lang1 = request.form['Language']
-		res = ""
-		for x in Languages:
-			if x[0] == lang1:
-				res = x[1]
-				break
-		translated = custom_convert(text, res)
-
-	return render_template('results.html', text=text, lang1=lang1, translated=translated, Languages=Languages)
+    if request.method == 'POST':
+        res = ""
+        text = request.form['lang']
+        lang1 = request.form['Language']
+        for x in Languages:
+            if x[0] == lang1:
+                res = x[1]
+                break
+        print(lang1, res)
+        if lang1 == 'secret':
+            res = 'en'
+            lang1 = "English"
+        translated = custom_convert(text, res)
+    return render_template('results.html', text=text, lang1=lang1, translated=translated, Languages=Languages)
 
 
 
