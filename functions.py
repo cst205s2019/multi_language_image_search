f5d8850e1574004c7a45a78579ee5bbf4f791b34
Michael Ridge <pikchu1998@gmail.com>
2019-05-12 21:16:32 -0700

    Fix

--- functions.py
+++ functions.py
@@ -1,19 +1,19 @@
 def custom_convert(text, lang):
     """
-    A function which uses the Google Cloud Vision API, creating a class 
-    and then calling the translate method. 
+    A function which uses the Google Cloud Vision API, creating a class
+    and then calling the translate method.
 
-    Keyword arguments:  
+    Keyword arguments:
     text -- the user input string to be translated
     lang -- the 2 letter abbreviation of a language
 
     """
+    from google.cloud import translate
 
-	from google.cloud import translate
 
-	# Instantiates a client
-	translate_client = translate.Client()
-	translation = translate_client.translate(
-	        text,
-	        target_language=lang)
-	return translation['translatedText']
+    # Instantiates a client
+    translate_client = translate.Client()
+    translation = translate_client.translate(
+            text,
+            target_language=lang)
+    return translation['translatedText']
